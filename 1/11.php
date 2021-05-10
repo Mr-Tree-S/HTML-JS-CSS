1a960733de554fdf356cfe6466fdbe1cb7780304
root <root@cs.cs>
2021-05-10 15:21:41 +0800

    html
    Signed-off-by: root <root@cs.cs>

---
+++ 1/11.php
@@ -0,0 +1,16 @@
+<?php
+
+$file = '1.txt';
+$user = $_POST['user'];
+
+file_put_contents($file,$user);
+
+
+
+/**
+echo "HELLO!!!  ";
+echo $_POST['user'];
+echo "<br>Your Password is  ";
+echo $_POST['pwd'];
+*/
+?>
